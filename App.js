908fd423f730e9f2527a666d8397f9d01312bb4a
Hesham Alatrash <heshamoov90@gmail.com>
2021-06-06 11:15:45 +0400

    Add some React Components

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    TouchableNativeFeedback, 
+    View, SafeAreaViewBase,
+    Image } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Pressed")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableNativeFeedback>
+        <View style={{
+          width: 200, height: 70, backgroundColor: 'dodgerblue'
+        }}>
+        </View>
+      </TouchableNativeFeedback>
+
+      <Text onPress={handlePress} numberOfLines={1}>Hello React Native!</Text>
+      <TouchableOpacity onPress={() => console.log('Image Tapped')}>
+        <Image 
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"
+        }}/>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
